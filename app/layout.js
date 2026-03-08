045a5834dc4b7903a84b77205253bf938178d4ae
Raksha Nayak <118899722+Rakshanayak24@users.noreply.github.com>
2026-03-08 16:34:56 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
-"use client";
+"use client"; // Must be client for interactivity
 
 import { CartProvider } from "../context/CartContext";
+import Header from "../components/Header";
 
 export default function RootLayout({ children }) {
   return (
@@ -8,8 +9,11 @@ export default function RootLayout({ children }) {
       <head>
         <title>Ecoyaan Checkout</title>
       </head>
-      <body>
-        <CartProvider>{children}</CartProvider>
+      <body className="bg-gray-100 min-h-screen">
+        <CartProvider>
+          <Header />  {/* Header will show login/logout/order buttons */}
+          <main>{children}</main>
+        </CartProvider>
       </body>
     </html>
   );
