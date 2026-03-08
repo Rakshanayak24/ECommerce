0ac7898b317eb15cf6846c2915eee86e9405cd7b
Raksha Nayak <118899722+Rakshanayak24@users.noreply.github.com>
2026-03-08 17:17:16 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,19 @@
 "use client";
 
 import { CartProvider } from "../context/CartContext";
+import Header from "../components/Header";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>Ecoyaan Checkout</title>
+        <title>Ecoyaan Store</title>
       </head>
-      <body>
-        <CartProvider>{children}</CartProvider>
+      <body className="bg-gray-100 min-h-screen">
+        <CartProvider>
+          <Header />
+          <main className="mt-4">{children}</main>
+        </CartProvider>
       </body>
     </html>
   );
